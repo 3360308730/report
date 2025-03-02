32eeefc74a4068e3796db192b06ce43549376032
cxj <3360308730@qq.com>
2025-03-02 21:43:08 +0800

    test Socket-plus

--- easychat-backend/src/main/java/com/easychat/test/SocketServer.java
+++ easychat-backend/src/main/java/com/easychat/test/SocketServer.java
@@ -1,26 +1,52 @@
 package com.easychat.test;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SocketServer {
     public static void main(String[] args) {
         ServerSocket server = null;
-        try{
+        Map<String, Socket> CLENT_MAP = new HashMap<>();
+        try {
             server = new ServerSocket(1024);
-            System.out.println("服务器已启动，等待客户端连接");
-            Socket socket = server.accept();
-            String ip = socket.getInetAddress().getHostAddress();
-            System.out.println("有客户端连接ip:"+ip+",端口:"+socket.getPort());
-            InputStream inputStream = socket.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String readData = bufferedReader.readLine();
-            System.out.println("收到的客户端信息为:"+readData);
-        }catch(Exception e){
+            System.out.println("服务已启动，等待客户端链接");
+            while (true) {
+                Socket socket = server.accept();
+                String ip = socket.getInetAddress().getHostAddress();
+                System.out.println("有客户端连接ip:" + ip + ",端口:" + socket.getPort());
+                String clientKey = ip + socket.getPort();
+                CLENT_MAP.put(clientKey, socket);
+                new Thread(() -> {
+                    while (true) {
+                        try {
+                            InputStream inputStream = socket.getInputStream();
+                            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf8");
+                            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                            String readData = bufferedReader.readLine();
+                            System.out.println("收到客户端消息->" + readData);
+
+                            CLENT_MAP.forEach((k, v) -> {
+                                try {
+                                    OutputStream outputStream = v.getOutputStream();
+                                    PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(outputStream, "utf8"));
+                                    printWriter.println(socket.getPort() + ":" + readData);
+                                    printWriter.flush();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            });
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
